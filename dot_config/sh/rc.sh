4b254e7f76fb5daa72bdedc379b5bc29d56574b9
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-11-24 20:35:11 +0000

    Update bashrc

--- dot_config/sh/rc.sh
+++ dot_config/sh/rc.sh
@@ -1,9 +1,11 @@
-## Variables
+# Variables
 local  THIS_DIR="$(dirname $(readlink -f "$0"))"
+
+# Nvim
 export EDITOR="nvim"
 
 # VS Code
-alias code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 
 # Github CLI
 export GH_TOKEN="op://Personal/GitHub/Credentials/token"
@@ -19,3 +21,7 @@ source <(fzf --zsh)
 
 # Chezmoi
 source <(chezmoi completion zsh)
+
+# Brew
+export HOMEBREW_BUNDLE_DUMP_DESCRIBE=y
+export HOMEBREW_BUNDLE_DUMP_NO_VSCODE=y
